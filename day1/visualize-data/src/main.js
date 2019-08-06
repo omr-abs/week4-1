ba4f91c0e404a0629634e8ea8e63c80e58096394
Patrick_B <pb5191@gmail.com>
2019-08-06 00:34:56 -0400

    filter feature done

--- day1/visualize-data/src/main.js
+++ day1/visualize-data/src/main.js
@@ -1,5 +1,5 @@
-const first = list[0];
 const main = document.querySelector(".main-elmnt");
+const result = document.querySelector(".result-zone");
 
 const initial = str => {
   return str[0].toUpperCase() + str.substring(1);
@@ -33,8 +33,40 @@ const createCard = (obj, indx) => {
 
   textCard.append(nameField, genderField, emailField);
   newCard.append(portrait, textCard);
-  main.appendChild(newCard);
+  result.appendChild(newCard);
 };
+
+const filterData = evnt => {
+  evnt.preventDefault();
+  const collection = document.querySelectorAll("input");
+  const selector = Array.from(collection)
+    .map(item => [item.id, item.value])
+    .filter(item => item[1])[0];
+
+  if (!selector) {
+    alert("Enter at least a criterion");
+    return;
+  } // Click with no value entered
+  const key = selector[0];
+  const val = selector[1];
+  let filteredList = [];
+
+  if (key === "gender") {
+    filteredList = list.filter(item => item.gender === val);
+  } else {
+    filteredList = list.filter(item => item.name[key] === val);
+  }
+
+  // removes cards for complete list
+  const cardList = document.querySelectorAll(".card");
+  for (card of cardList) {
+    result.removeChild(card);
+  }
+  loopCreate(filteredList);
+  document.querySelector(`#${key}`).value = null;
+};
+
+document.querySelector("#submitButton").addEventListener("click", filterData);
 
 const loopCreate = arr => {
   for (let i = 0; i < arr.length; i++) {
